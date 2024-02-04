bc1e3579abcb9f80cbabd049261ff310627f77c9
Hlib Dmytriiev <action.dmitrieff@gmail.com>
2024-02-04 16:00:46 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const input = document.querySelector("input");
+const defaultText = document.getElementById("default");
+const debounceText = document.getElementById("debounce");
+
+const updateDebounceText = debounce((text) => {
+  debounceText.textContent = text;
+});
+
+input.addEventListener("input", (e) => {
+  defaultText.textContent = e.target.value;
+  updateDebounceText(e.target.value);
+});
+
+function debounce(cb, delay = 1000) {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      cb(...args);
+    }, delay);
+  };
+}
